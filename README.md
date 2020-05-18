ac294ae00a62ccfc1b9c6b9baad1e1afa236bbca
DevMo-13 <moniquecinatl@gmail.com>
2020-05-18 16:16:17 -0700

    updated js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project 2 - List Pagination and Filtering
  List Pagination and Filtering
+
+ This project uses JavaScript to take a list of student info and only displays 10 students on the page at a time. Navigation capability is added using numbered links at the bottom of the page so users can click through each page to see the other students' info.
--- js/script.js
+++ js/script.js
@@ -1,50 +1,81 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+--aiming for meets expectations--
 ******************************************/
+
+const studentListItems = document.querySelectorAll('.student-item');
+const itemsPerPage = 10;
+
+/** 
+   `showPage` function loops through studentListItems
+   and hides all of the items in the list except for the 
+   10 to be shown on each given page.
+   @param {array} list - The list of students being passed into the function
+   @param {number} page - The page number being passed in
+**/
+
+function showPage(list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
+      };
+   };
+};
+
+/**
+   `appendPageLinks` function generates, appends, 
+   and adds functionality to the pagination buttons.
+   @param {array} list - The list of students being passed into the function
+**/
+
+function appendPageLinks(list) {
+   //a new div is created and appended to the existing
+   //pageDiv and given a class name 'pagination'
+   const pageDiv = document.getElementsByClassName('page')[0];
+   const paginationDiv = document.createElement('div');
+   paginationDiv.className = 'pagination';
+   pageDiv.appendChild(paginationDiv);
+  
+   //an unordered list is created and appended to the new paginationDiv
+   const paginationUl = document.createElement('ul')
+   paginationDiv.appendChild(paginationUl);
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+   const numberofPages = Math.ceil(list.length / itemsPerPage);
+   for (let i = 0; i < numberofPages; i++) {
+      //for each page needed, a new list item is created and appended to the paginationUl
+      let paginationLi = document.createElement('li');
+      paginationUl.appendChild(paginationLi);
+      
+      //for each paginationLi created, a new anchor tag is created and nested under the li, 
+      //given an href attribute, and assigned text that displays the corresponding page number
+      let paginationA = document.createElement('a');
+      paginationA.setAttribute('href', '#');
+      paginationA.textContent = `${i+1}`;
+      paginationLi.appendChild(paginationA);
+      
+      //the first paginationA is assigned the class name 'active'
+      if (i === 0) {
+         paginationA.className = "active";
+      };   
+      
+      //each paginationA is assigned an event listener that
+      // -removes the class 'active' from the buttons that were not clicked
+      // -adds the 'active' class to the button that was clicked
+      // -calls the showPage function to only display certain students
+      paginationA.addEventListener('click', (e) => {
+         let paginationButtons = document.getElementsByTagName('a');
+         for (let i = 0; i < paginationButtons.length; i++) {
+            paginationButtons[i].classList.remove('active');
+         };
+         e.target.className = 'active';
+         showPage(studentListItems, parseInt(e.target.textContent));         
+      });
+   };
+};
+showPage(studentListItems, 1);
+appendPageLinks(studentListItems);
